cbf37970f1ad368faf0496045ec76d17683a9fb6
apb <apb@apbs-MBP.apb.homelab.com>
2024-05-27 21:42:41 -0500

    add marksman to lsp config

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -191,6 +191,7 @@ return {
         pyright = {},
         -- rust_analyzer = {},
         html = {},
+        marksman = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
