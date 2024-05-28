db1918e85cfe0a02042ca97b54b588f3bc4ec40b
apb <apb@apbs-MBP.apb.homelab.com>
2024-05-27 20:13:13 -0500

    add rose pine theme

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -41,7 +41,7 @@ require('lazy').setup({
 
   require 'kickstart/plugins/cmp',
 
-  require 'kickstart/plugins/tokyonight',
+  -- require 'kickstart/plugins/tokyonight',
 
   require 'kickstart/plugins/todo-comments',
 
