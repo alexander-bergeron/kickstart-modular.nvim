0607af256f8f6593da63261b13e61a20224bc9aa
apb <apb@apbs-MacBook-Pro.local>
2024-05-29 09:11:53 -0500

    add spell check and some lsp servers

--- init.lua
+++ init.lua
@@ -93,6 +93,10 @@ vim.g.maplocalleader = ' '
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
 
+-- Turn on spell check
+vim.opt.spelllang = 'en_us'
+vim.opt.spell = true
+
 -- [[ Setting options ]]
 require 'options'
 
--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -189,9 +189,11 @@ return {
           },
         },
         pyright = {},
+        -- postgres_lsp = {},
         -- rust_analyzer = {},
         html = {},
         marksman = {},
+        -- sqls = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
