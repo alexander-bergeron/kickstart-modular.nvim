61309f21f089d9331a912419df4dcee4996679ff
Alexander Bergeron <alexander-bergeron@github.com>
2024-06-03 18:55:14 -0500

    added additional lsps and change complete to CR rather than C-y

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -189,11 +189,10 @@ return {
           },
         },
         pyright = {},
-        -- postgres_lsp = {},
         -- rust_analyzer = {},
         html = {},
         marksman = {},
-        -- sqls = {},
+        sqlls = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
